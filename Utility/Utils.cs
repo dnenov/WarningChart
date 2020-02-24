ffc8da02feea1817e6fb1c9ff345d25678031e28
Deyan Nenov <dnenov@archilizer.com>
2020-02-24 22:09:31 +0000

    Upgrade to Revit 2021

--- Utility/Utils.cs
+++ Utility/Utils.cs
@@ -10,77 +10,7 @@ using WC.WarningChartWPF;
 namespace WC
 {
     public static class Utils
-    {
-        private static DisplayUnitType dut;
-        public const double METERS_IN_FEET = 0.3048;
-
-        public static void Init(Document doc)
-        {
-            dut = doc.GetUnits().GetFormatOptions(UnitType.UT_Length).DisplayUnits;
-        }
-        /// <summary>
-        /// forward conversion of project to unit values
-        /// </summary>
-        /// <param name="p"></param>
-        /// <returns></returns>
-        public static double convertValueTO(double p)
-        {
-            switch (dut)
-            {
-                case DisplayUnitType.DUT_METERS:
-                    return p * METERS_IN_FEET;
-                case DisplayUnitType.DUT_CENTIMETERS:
-                    return p * METERS_IN_FEET * 100;
-                case DisplayUnitType.DUT_DECIMAL_FEET:
-                    return p;
-                case DisplayUnitType.DUT_DECIMAL_INCHES:
-                    return p * 12;
-                case DisplayUnitType.DUT_METERS_CENTIMETERS:
-                    return p * METERS_IN_FEET;
-                case DisplayUnitType.DUT_MILLIMETERS:
-                    return p * METERS_IN_FEET * 1000;
-                case DisplayUnitType.DUT_DECIMETERS:
-                    return p * METERS_IN_FEET * 10;
-            }
-            return p;
-        }
-        /// <summary>
-        /// reverse the unit transformation to project units
-        /// </summary>
-        /// <param name="p"></param>
-        /// <returns></returns>
-        public static double convertValueFROM(double p)
-        {
-            switch (dut)
-            {
-                case DisplayUnitType.DUT_METERS:
-                    return p / METERS_IN_FEET;
-                case DisplayUnitType.DUT_CENTIMETERS:
-                    return p / METERS_IN_FEET / 100;
-                case DisplayUnitType.DUT_DECIMAL_FEET:
-                    return p;
-                case DisplayUnitType.DUT_DECIMAL_INCHES:
-                    return p / 12;
-                case DisplayUnitType.DUT_METERS_CENTIMETERS:
-                    return p / METERS_IN_FEET;
-                case DisplayUnitType.DUT_MILLIMETERS:
-                    return p / METERS_IN_FEET / 1000;
-                case DisplayUnitType.DUT_DECIMETERS:
-                    return p / METERS_IN_FEET / 10;
-            }
-            return p;
-        }
-        /// <summary>
-        /// check if we support user units
-        /// </summary>
-        /// <returns></returns>
-        public static Boolean _goUnits()
-        {
-            if (dut.Equals(DisplayUnitType.DUT_FEET_FRACTIONAL_INCHES)) return false;
-            if (dut.Equals(DisplayUnitType.DUT_FRACTIONAL_INCHES)) return false;
-            return true;
-        }
-
+    {      
         internal static Tuple<List<WarningChartModel>, List<WarningChartModel>, List<WarningChartModel>> FindChange(List<WarningChartModel> previousWarningModels, List<WarningChartModel> warningModels)
         {
             if(previousWarningModels == null)
